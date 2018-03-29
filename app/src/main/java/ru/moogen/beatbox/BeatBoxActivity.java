0384adb450252ebb678d887e84143f00e3e87a37
Moogen <moongametv@gmail.com>
2018-03-29 13:54:22 +0300

    add singlefragment

--- app/src/main/java/ru/moogen/beatbox/BeatBoxActivity.java
+++ app/src/main/java/ru/moogen/beatbox/BeatBoxActivity.java
@@ -1,9 +1,15 @@
 package ru.moogen.beatbox;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class BeatBoxActivity extends AppCompatActivity {
+public class BeatBoxActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return BeatBoxFragment.newInstance();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/ru/moogen/beatbox/BeatBoxFragment.java
+++ app/src/main/java/ru/moogen/beatbox/BeatBoxFragment.java
@@ -1,10 +1,28 @@
 package ru.moogen.beatbox;
 
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ru.moogen.beatbox.databinding.FragmentBeatBoxBinding;
 
 public class BeatBoxFragment extends Fragment {
 
     public static BeatBoxFragment newInstance() {
         return new BeatBoxFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        FragmentBeatBoxBinding binding = DataBindingUtil.inflate(inflater
+                , R.layout.fragment_beat_box, container, false);
+        return binding.getRoot();
     }
 }
