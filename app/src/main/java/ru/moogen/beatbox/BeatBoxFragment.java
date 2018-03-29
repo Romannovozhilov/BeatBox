5464aec66e784d1acfc2dd3b9708d2a795f76832
Moogen <moongametv@gmail.com>
2018-03-29 14:54:44 +0300

    add sounds

--- app/src/main/java/ru/moogen/beatbox/BeatBoxFragment.java
+++ app/src/main/java/ru/moogen/beatbox/BeatBoxFragment.java
@@ -5,11 +5,14 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import ru.moogen.beatbox.databinding.FragmentBeatBoxBinding;
+import ru.moogen.beatbox.databinding.ListItemSoundBinding;
 
 public class BeatBoxFragment extends Fragment {
 
@@ -23,6 +26,40 @@ public class BeatBoxFragment extends Fragment {
 
         FragmentBeatBoxBinding binding = DataBindingUtil.inflate(inflater
                 , R.layout.fragment_beat_box, container, false);
+        binding.recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        binding.recyclerView.setAdapter(new SoundAdapter());
+
         return binding.getRoot();
+    }
+
+    private class SoundHolder extends RecyclerView.ViewHolder{
+        private ListItemSoundBinding mBinding;
+
+        public SoundHolder(ListItemSoundBinding binding) {
+            super(binding.getRoot());
+            mBinding = binding;
+        }
+    }
+
+    private class SoundAdapter extends  RecyclerView.Adapter<SoundHolder>{
+
+        @NonNull
+        @Override
+        public SoundHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(getActivity());
+            ListItemSoundBinding binding = DataBindingUtil.inflate(inflater,
+                    R.layout.list_item_sound, parent, false);
+            return new SoundHolder(binding);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull SoundHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
     }
 }
