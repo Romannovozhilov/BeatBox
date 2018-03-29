97212e750e160b7512a75ee434ab375e23d2903e
Moogen <moongametv@gmail.com>
2018-03-29 13:11:07 +0300

    add singlefragment

---
+++ app/src/main/java/ru/moogen/beatbox/BeatBoxFragment.java
@@ -0,0 +1,10 @@
+package ru.moogen.beatbox;
+
+import android.support.v4.app.Fragment;
+
+public class BeatBoxFragment extends Fragment {
+
+    public static BeatBoxFragment newInstance() {
+        return new BeatBoxFragment();
+    }
+}
